4d46e9e438d693058019c097f9f640f73f78f871
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-11-29 12:14:37 +0100

    Wrong message style

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -5,7 +5,7 @@ function CharacterList({characters, filterName}) {
     //console.log(characters);
     //console.log(filterName);
   if (characters.length === 0){
-    return <p>No hay personajes que coincidan con la búsqueda {filterName}</p>
+    return <p className="characterList_message">No hay personajes que coincidan con la búsqueda: {filterName}</p>
   }
 
 // const characterItems = characters.map((character) => {
