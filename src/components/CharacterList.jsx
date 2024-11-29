9bc28a09c9fa84347c528e3e8fad0b83d9ac238d
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-11-29 11:35:44 +0100

    Sort alphabetically

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -7,11 +7,23 @@ function CharacterList({characters, filterName}) {
   if (characters.length === 0){
     return <p>No hay personajes que coincidan con la búsqueda {filterName}</p>
   }
+
+// const characterItems = characters.map((character) => {
+//     //console.log(character);
+//   return <CharacterCard characterData={character} key={character.id} />
+// })
     
-    const characterItems = characters.map((character) => {
-        //console.log(character);
-        return <CharacterCard characterData={character} key={character.id} />
-    })    
+    const characterItems = characters
+      .sort((a,b) => {
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+      return 0;
+      })
+      .map((character) => { 
+      return <CharacterCard characterData={character} key={character.id} />
+      });
+
+     console.log(characterItems);    
 
   return (
     <ul className="main_list">
